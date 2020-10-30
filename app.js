35ce6f86a664a3a94b9dc00e9ec935851033c373
brianneewell1 <70183030+brianneewell1@users.noreply.github.com>
2020-10-30 17:18:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,76 +1,33 @@
-// select all elements and create variables
+  
+//youtube api call
+$(document).ready(function () {
+  $('#search-term').click(function (event) {
+      event.preventDefault();
+      var searchTerm = $('#query').val();
+      getRequest(searchTerm);
+  });
+});
 
-//first page
-var firstPage = document.getElementById("firstPage");
-var continueBtn = document.getElementById("continueBtn");
+function getRequest(searchTerm) {
+  var url = 'https://www.googleapis.com/youtube/v3/search';
+  var params = {
+      part: 'snippet',
+      key: 'AIzaSyAZbcpLkq2DEmBTLkQX-3XeqTcfi4k83qQ',
+      q: searchTerm
+  };
 
-//progress bar
-var myProgress = document.getElementById("myProgress");
-var myBar = document.getElementById("myBar");
+  $.getJSON(url, params, showResults);
+}
 
-//music category
-var category = document.getElementById("category");
-var countryBtn = document.getElementById("countryBtn");
-var jazzBtn = document.getElementById("jazzBtn");
-var rockBtn = document.getElementById("rockBtn");
-var electronicBtn = document.getElementById("electronicBtn");
-
-//country
-var country = document.getElementById("country");
-var countryOne = document.getElementById("countryOne");
-var countryTwo = document.getElementById("countryTwo");
-var countryThree = document.getElementById("countryThree");
-var countryFour = document.getElementById("countryFour");
-
-//Jazz
-var jazz = document.getElementById("jazz");
-var jazzOne = document.getElementById("jazzOne");
-var jazzTwo = document.getElementById("jazzTwo");
-var jazzThree = document.getElementById("jazzThree");
-var jazzFour = document.getElementById("jazzFour");
-
-//Rock
-var rock = document.getElementById("rock");
-var rockOne = document.getElementById("rockOne");
-var rockTwo = document.getElementById("rockTwo");
-var rockThree = document.getElementById("rockThree");
-var rockFour = document.getElementById("rockFour");
-
-//Electronic 
-var electronic = document.getElementById("electronic");
-var elecOne = document.getElementById("elecOne");
-var elecTwo = document.getElementById("elecTwo");
-var elecThree = document.getElementById("elecThree");
-var elecFour = document.getElementById("elecFour");
-
-//Timeline
-var timeline = document.getElementById("timeline");
-var myRange = document.getElementById("myRange");
-
-//Footer
-var thanksBtn = document.getElementById("thanksBtn");
-
-//hide other pages upon loading
-function initPage() {
-  firstPage.style.display ="block";
-  myProgress.style.display = "none";
-  category.style.display = "none";
-  country.style.display = "none";
-  rock.style.display = "none";
-  jazz.style.display = "none";
-  electronic.style.display = "none";
-  }
-initPage();
-
-//display music categories when continue is clicked
-continueBtn.addEventListener("click", showMusic);
-
-function showMusic() {
-firstPage.style.display = "none";
-myProgress.style.display = "block";
-category.style.display = "block";
-country.style.display = "none";
-rock.style.display = "none";
-jazz.style.display = "none";
-electronic.style.display = "none";
+function showResults(results) {
+  var html = "";
+  var entries = results.items;
+  
+  $.each(entries, function (index, value) {
+      var title = value.snippet.title;
+      var thumbnail = value.snippet.thumbnails.default.url;
+      html += '<p>' + title + '</p>';
+      html += '<img src="' + thumbnail + '">';
+  }); 
+  $('#search-results').html(html);
 }
